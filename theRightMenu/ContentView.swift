609ac42a8150eacd3248a68eca4c64d3f74863ad
Reom <reomnagasaka@gmail.com>
2026-04-29 15:40:52 +0900

    Initial Commit

---
+++ theRightMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  theRightMenu
+//
+//  Created by Reom Nagasaka on 2026/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ theRightMenu/theRightMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  theRightMenuApp.swift
+//  theRightMenu
+//
+//  Created by Reom Nagasaka on 2026/04/29.
+//
+
+import SwiftUI
+
+@main
+struct theRightMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
